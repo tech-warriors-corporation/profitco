c1b6620bdddd2937ddfbdab17189950053330857
hotequil <hotequil@protonmail.com>
2022-04-08 22:42:11 -0300

    feat: updated name methods

--- utils.py
+++ utils.py
@@ -1,12 +1,12 @@
 months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
 
-def show_space():
+def print_space():
     print("")
 
-def show_bar():
-    show_space()
+def print_bar():
+    print_space()
     print("------------------------------------------------------------------------------")
-    show_space()
+    print_space()
 
-def format_value(value):
+def format_money_value(value):
     return "R$ {:.2f}".format(value)
