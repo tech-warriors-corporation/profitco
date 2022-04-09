96c4f8d818d83b0001b548f09d3744507769c13c
hotequil <hotequil@protonmail.com>
2022-04-09 11:59:28 -0300

    feat: created new methods

--- utils.py
+++ utils.py
@@ -10,3 +10,24 @@ def print_bar():
 
 def format_money_value(value):
     return "R$ {:.2f}".format(value)
+
+def calculate_price_media_of_months(data, key):
+    media = 0
+
+    for month in months:
+        media += data[month][key]
+
+    media /= len(months)
+
+    return media
+
+def calculate_total_of_months(data, key):
+    total = 0
+
+    for month in months:
+        total += data[month][key]
+
+    return total
+
+def is_it_profit_or_loss(value):
+    return ('prejuízo', 'lucro')[value >= 0]
